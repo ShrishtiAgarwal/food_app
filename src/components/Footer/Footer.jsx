1af3c140956b5f07b362d940bb262dbd15c50fd7
ShrishtiAgarwal <shrishtiagarwal21@gmail.com>
2022-09-29 10:20:55 +0530

    footer implementation started

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Row, Col, Container, ListGroup, List, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
 
 const footer = () => {
-    return (
-        <div>
-            footer
-        </div>
-    );
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+          <div className="logo">
+            {" "}
+            <img src={logo} alt="logo" />
+            <h5>Tasty Food</h5>
+          </div>
+            </Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+          <Col lg="3" md="4" sm="6"></Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default footer;
