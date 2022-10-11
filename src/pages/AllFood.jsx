ca839da6f9d65ac971c71a800ed5559577782017
ShrishtiAgarwal <shrishtiagarwal21@gmail.com>
2022-10-11 21:51:03 +0530

    implemented searching

--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
 import products from "../assets/fake-data/products";
 import Helmet from "../components/Helmet/helmet";
 import CommonSection from "../components/UI/CommonSection";
 import Product from "../components/UI/Product";
-import '../styles/all-foods.css'
+import "../styles/all-foods.css";
 
 const AllFood = () => {
+  const [searchItem, setSearchItem] = useState("");
+
   return (
     <Helmet title={"All Foods"}>
       <CommonSection title={"All Foods"}></CommonSection>
@@ -15,7 +17,14 @@ const AllFood = () => {
           <Row>
             <Col lg="6" md="6" sm="6">
               <div className="search__widget d-flex align-items-center justify-content-between w-50">
-                <input type="text" placeholder="I'm looking for ..."></input>
+                <input
+                  type="text"
+                  placeholder="I'm looking for ..."
+                  value={searchItem}
+                  onChange={(e) => {
+                    setSearchItem(e.target.value);
+                  }}
+                ></input>
                 <span>
                   <i class="ri-search-line"></i>
                 </span>
@@ -23,7 +32,7 @@ const AllFood = () => {
             </Col>
             <Col lg="6" md="6" sm="6" className="mb-5">
               <div className="data_widget text-end">
-                <select>
+                <select className="w-50">
                   <option>Default</option>
                   <option value="ascending">Alphabetically, A-Z</option>
                   <option value="descending">Alphabetically, Z-A</option>
@@ -32,11 +41,16 @@ const AllFood = () => {
                 </select>
               </div>
             </Col>
-            {products.map((item) => (
-              <Col lg="3" md="4" sm="6" className="mb-4">
-                <Product item={item} key={item.id} />
-              </Col>
-            ))}
+            {products
+              .filter((product) => {
+                if (searchItem === "") return product;
+                return product.title.toLowerCase().includes(searchItem);
+              })
+              .map((item) => (
+                <Col lg="3" md="4" sm="6" className="mb-4">
+                  <Product item={item} key={item.id} />
+                </Col>
+              ))}
           </Row>
         </Container>
       </section>
